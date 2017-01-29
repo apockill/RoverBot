9f706e4c9ce63f1c897d29e5906456c691166902
apockill <apocthiel@gmail.com>
2017-01-28 18:38:35 -0700

    Transfering code to RasPi

--- Robot.py
+++ Robot.py
@@ -126,7 +126,7 @@ class Encoder:
         if pin == self.pinB: newPinB = int(not newPinB)
 
         self.addLogEntry(newPinA, newPinB)
-        print(str(self.log[-1].A) + str(self.log[-1].B) + " " + str(self.log[-1].vel) + " \t" + str(round(self.log[-1].time, 3)) )
+
 
     def addLogEntry(self, newPinA, newPinB):
         """
@@ -159,3 +159,4 @@ class Encoder:
                                  acc   = 0)
         self.log.append(newEntry)
 
+        print(str(newEntry.A) + str(newEntry.B) + " " + str(newEntry.vel) + " \t" + elapsedTime)
