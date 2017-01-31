f850e65834db27cff9022e56ad88b0e42d3ced0b
apockill <apocthiel@gmail.com>
2017-01-30 18:56:52 -0700

    Transfering code to RasPi

--- Video.py
+++ Video.py
@@ -29,3 +29,5 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     # if the `q` key was pressed, break from the loop
     if key == ord("q"):
         break
+
+    # Test
