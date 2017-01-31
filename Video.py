eb561115a5a1aa1704fda4e2cf3561b66732bbba
apockill <apocthiel@gmail.com>
2017-01-30 18:49:41 -0700

    Transfering code to RasPi

--- Video.py
+++ Video.py
@@ -1,18 +1,31 @@
+# import the necessary packages
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 import cv2
 
+# initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
-rawCapture = PiRGBArray(camera)
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
 
-# Let the camera warm up
+# allow the camera to warmup
 time.sleep(0.1)
 
-# Get image from camera
-camera.capture(rawCapture, format="bgr")
-image = rawCapture.array
+# capture frames from the camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    # grab the raw NumPy array representing the image, then initialize the timestamp
+    # and occupied/unoccupied text
+    image = frame.array
 
-# Display the image
-cv2.imshow("Image", image)
-cv2.waitKey(0)
+    # show the frame
+    cv2.imshow("Frame", image)
+    key = cv2.waitKey(1) & 0xFF
+
+    # clear the stream in preparation for the next frame
+    rawCapture.truncate(0)
+
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
