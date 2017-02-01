9b2229a5c48874f7c4289a8b0e6b5b1e7050852b
apockill <apocthiel@gmail.com>
2017-01-31 21:56:02 -0700

    Transfering code to RasPi

--- Robot.py
+++ Robot.py
@@ -180,6 +180,7 @@ class Encoder:
     def getVelocity(self):
         sampleSize = 5
         if len(self.log) < sampleSize: sampleSize = len(self.log)
+        if sampleSize == 0: return 0
 
         old         = self.log[-sampleSize]
         ticks       = self.count - old.count
