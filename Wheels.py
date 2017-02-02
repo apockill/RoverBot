e9f044dc90d5310c8e95db5def0bb135550b2454
apockill <apocthiel@gmail.com>
2017-02-02 15:18:44 -0700

    Transfering code to RasPi

--- Wheels.py
+++ Wheels.py
@@ -204,7 +204,7 @@ class Encoder:
 
 
     def getVelocity(self):
-        sampleSize = 2
+        sampleSize = 6
         if len(self.log) < sampleSize + 1: sampleSize = len(self.log)
 
         old = self.log[-sampleSize]
