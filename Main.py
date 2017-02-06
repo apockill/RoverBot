c54a34ce6b1be335cc92c3171e2e54085aa1a310
apockill <apocthiel@gmail.com>
2017-02-05 21:59:09 -0700

    Transfering code to RasPi

--- Main.py
+++ Main.py
@@ -24,6 +24,7 @@ if __name__ == "__main__":
 
 
     robot.LWheel.setSpeed(150)
+    robot.RWheel.setSpeed(150)
     sleep(15)
     print("Final: ", robot.LWheel.encoder.getVelocity(sampleSize=50))
     # # robot.RWheel.setSpeed(100)
