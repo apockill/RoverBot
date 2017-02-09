adb0bb5f8ef05dd13c5f1873f8fd63c324c12a6b
apockill <apocthiel@gmail.com>
2017-02-08 18:49:26 -0700

    Transfering code to RasPi

--- Wheels.py
+++ Wheels.py
@@ -129,10 +129,11 @@ class Wheel(HardwareLoop):
         # Constants
         maxPowerChange = 25 * self.delay  # Power Change / Seconds
         # Works, but slow
-        kP = 0.05
-        kD = 0
+        kP = 0.005
         # kD = 0.02
+        kD = 0
 
+        # Other Options:
         # kP = 0.015
         # kD = 0.07
 
