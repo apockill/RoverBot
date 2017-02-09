dd11703046bcba8cbd9d57b83ef2aa63de94d5dc
apockill <apocthiel@gmail.com>
2017-02-08 18:40:03 -0700

    Transfering code to RasPi

--- Wheels.py
+++ Wheels.py
@@ -268,7 +268,7 @@ class Encoder:
         # Run the Callback Function for the parent
         self.getVelocity()
 
-    def getVelocity(self, sampleSize=1):
+    def getVelocity(self, sampleSize=2):
         if len(self.log) < sampleSize + 1: sampleSize = len(self.log)
         if sampleSize == 1: return 0
 
