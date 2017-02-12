af6c952f7376e0e222f62b2530a97405caa53c41
apockill <apocthiel@gmail.com>
2017-02-12 15:29:04 -0700

    Transfering code to RasPi

--- HardwareLibs/Camera.py
+++ HardwareLibs/Camera.py
@@ -62,8 +62,8 @@ class PanTiltPiCamera(PiVideoStream):
         startServos()
 
     def setPose(self, pan, tilt):
-        self.setServo(self.panPin, pan)
-        self.setServo(self.tiltPin, tilt)
+        setServo(self.panPin, pan)
+        setServo(self.tiltPin, tilt)
 
     def close(self):
         super().close()
