7f97e6b7235eb45e85117212651dd68630eb7b81
apockill <apocthiel@gmail.com>
2017-02-12 15:54:45 -0700

    Transfering code to RasPi

--- Main.py
+++ Main.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     camera.start()
     camera.setPose(0, 0)
     sleep(1)
-    camera.setPose(90, 90)
+    camera.setPose(45, 45)
     sleep(1)
     camera.setPose(0, 0)
     cv2.imshow('frame', camera.read())
