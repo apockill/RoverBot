149bf61c632df26b3be35ca4df723e571946eea3
apockill <apocthiel@gmail.com>
2017-02-12 18:13:55 -0700

    Transfering code to RasPi

--- Behaviors.py
+++ Behaviors.py
@@ -20,7 +20,7 @@ class FollowLine:
         img   = self.rover.camera.read()
 
         # gray  = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        rImg = img[:,:,0]
+        rImg = img[:,:,2]
         ret, rThresh = cv2.threshold(rImg, 90, 255, cv2.THRESH_BINARY)
         edges = cv2.Canny(rThresh, 20, 35)
 
@@ -31,7 +31,7 @@ class FollowLine:
         lines = cv2.HoughLines(edges, 1, np.pi / 180, 200)
         if lines is None:
             cv2.imshow('Edge', edges)
-            cv2.imshow('Gray', rThresh)
+            cv2.imshow('Gray', rImg)
             # cv2.imshow('Frame', gray)
             cv2.waitKey(500)
             return
