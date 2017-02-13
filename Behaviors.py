40081592d0bd249a5990833a34f2aa932bb2d1fb
apockill <apocthiel@gmail.com>
2017-02-12 18:03:20 -0700

    Transfering code to RasPi

--- Behaviors.py
+++ Behaviors.py
@@ -20,7 +20,7 @@ class FollowLine:
         img   = self.rover.camera.read()
         gray  = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        edges = cv2.Canny(gray, 45, 50)
+        edges = cv2.Canny(gray, 15, 50)
 
 
 
