3785462978fdc2d23035d3b2a208218d7027c398
apockill <apocthiel@gmail.com>
2017-02-12 18:37:05 -0700

    Transfering code to RasPi

--- Behaviors.py
+++ Behaviors.py
@@ -35,22 +35,27 @@ class FollowLine:
         cv2.imshow('r', rThresh)
         cv2.imshow('e', edges)
 
-        lines = cv2.HoughLines(rThresh, 1, np.pi / 180, 200)
+        lines = cv2.HoughLinesP(image=rThresh, rho=0.02, theta=np.pi/500, threshold=10, minLineLength=50) # 1, np.pi / 180, 200)
         if lines is None:
             # cv2.imshow('Frame', gray)
             cv2.waitKey(3000)
             return
 
-        for line in lines[:10]:
-            for rho, theta in line:
-                a = np.cos(theta)
-                b = np.sin(theta)
-                x0 = a * rho
-                y0 = b * rho
-                x1 = int(x0 + 1000 * (-b))
-                y1 = int(y0 + 1000 * (a))
-                x2 = int(x0 - 1000 * (-b))
-                y2 = int(y0 - 1000 * (a))
+        a, b, c = lines.shape
+        for i in range(a):
+            cv2.line(img, (lines[i][0][0], lines[i][0][1]), (lines[i][0][2], lines[i][0][3]), (0, 0, 255), 3, cv2.LINE_AA)
+
+
+                # for line in lines[:10]:
+        #     for rho, theta in line:
+        #         a = np.cos(theta)
+        #         b = np.sin(theta)
+        #         x0 = a * rho
+        #         y0 = b * rho
+        #         x1 = int(x0 + 1000 * (-b))
+        #         y1 = int(y0 + 1000 * (a))
+        #         x2 = int(x0 - 1000 * (-b))
+        #         y2 = int(y0 - 1000 * (a))
 
                 cv2.line(rImg, (x1, y1), (x2, y2), (0, 0, 255), 2)
         print("Found lines: ", len(lines))
