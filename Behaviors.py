15a69cef3c411c6799458dfcf3e2c7e58cca5744
apockill <apocthiel@gmail.com>
2017-02-13 15:41:14 -0700

    Transfering code to RasPi

--- Behaviors.py
+++ Behaviors.py
@@ -59,10 +59,10 @@ class FollowLine:
         lines = cv2.HoughLinesP(edges, 1, np.pi/180, threshold=100 , minLineLength=50, maxLineGap=100)
 
         if lines is not None:
-            print(lines)
-            lines = lines[0].tolist()
+            # print(lines)
 
-            for x1, y1, x2, y2 in lines:
+            for line in lines:
+                x1, y1, x2, y2 = line
                 cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 1)
 
             # Debug
