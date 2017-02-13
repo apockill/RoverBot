19a74cf124874db55befc6c8dd2fe1306a341919
apockill <apocthiel@gmail.com>
2017-02-13 12:38:57 -0700

    Transfering code to RasPi

--- VisionUtils.py
+++ VisionUtils.py
@@ -14,18 +14,31 @@ def autoCanny(image, sigma=0.33):
     # return the edged image
     return edged
 
+
 def isolateColor(img, lower, upper):
     """
-
     :param img: Image to isolate teh color of
     :param lower: [lowerHue, lowerSat, lowerVal]
     :param upper: [upperHue, upperSat, upperVal]
     :return: Isolated image
     """
+    # [160, 50, 50], [10, 255, 255]
+    # If the HSV values wrap around:
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    if lower[0] > upper[0]:
+        # [160, 50, 50] -> [180, 255, 255]
+        upper1 = [180, upper[1], upper[2]]
+        mask1  = cv2.inRange(hsv, np.array(lower), np.array(upper1))
 
-    # Isolate the red
-    hsv   = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    mask  = cv2.inRange(hsv, np.array(lower), np.array(upper))
+        # [0, 50, 50] -> [10, 255, 255]
+        lower2 = [0, lower[1], lower[2]]
+        mask2  = cv2.inRange(hsv, np.array(lower2), np.array(upper))
+
+        mask = mask1 + mask2
+
+    else:
+        mask  = cv2.inRange(hsv, np.array(lower), np.array(upper))
+
+
     final = cv2.bitwise_and(img, img, mask=mask)
-
     return final
