85bac43849eb1efecd614a309aad38676f281401
apockill <apocthiel@gmail.com>
2017-02-12 18:01:12 -0700

    Transfering code to RasPi

--- Behaviors.py
+++ Behaviors.py
@@ -33,17 +33,18 @@ class FollowLine:
             cv2.waitKey(500)
             return
 
-        for rho, theta in lines[0]:
-            a = np.cos(theta)
-            b = np.sin(theta)
-            x0 = a * rho
-            y0 = b * rho
-            x1 = int(x0 + 1000 * (-b))
-            y1 = int(y0 + 1000 * (a))
-            x2 = int(x0 - 1000 * (-b))
-            y2 = int(y0 - 1000 * (a))
+        for line in lines:
+            for rho, theta in line:
+                a = np.cos(theta)
+                b = np.sin(theta)
+                x0 = a * rho
+                y0 = b * rho
+                x1 = int(x0 + 1000 * (-b))
+                y1 = int(y0 + 1000 * (a))
+                x2 = int(x0 - 1000 * (-b))
+                y2 = int(y0 - 1000 * (a))
 
-            cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+                cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
         print("Found lines: ", len(lines))
 
         cv2.imshow('Edge', img)
