9128224005047770d62b948e9f61380b4f6f50fa
apockill <apocthiel@gmail.com>
2017-02-13 18:53:04 -0700

    Transfering code to RasPi

--- Behaviors.py
+++ Behaviors.py
@@ -30,7 +30,7 @@ class FollowLine:
         rGray = cv2.cvtColor(rImg, cv2.COLOR_BGR2GRAY)
         ret, rThresh = cv2.threshold(rGray, 50, 255, cv2.THRESH_BINARY)
 
-
+        # Resize
         small = cv2.resize(rThresh,    (9, 12), interpolation=cv2.INTER_AREA)
 
         # Debug
@@ -46,9 +46,14 @@ class FollowLine:
         rImg  = VisionUtils.isolateColor(img,   [150, 75, 75],  [30, 255, 255])
         rGray = cv2.cvtColor(rImg, cv2.COLOR_BGR2GRAY)
         ret, rThresh = cv2.threshold(rGray, 50, 255, cv2.THRESH_BINARY)
-        edges = cv2.Canny(rThresh, 20, 40)
 
-        cv2.imshow('Thresh', rThresh)
+        small = cv2.resize(rThresh, (9, 12), interpolation=cv2.INTER_AREA)
+        big = cv2.resize(small, (640, 480), interpolation=cv2.INTER_AREA)  # Delete- for debug only
+
+        edges = cv2.Canny(big, 20, 40)
+
+
+        cv2.imshow('Thresh', edges)
 
 
         # lines = cv2.HoughLinesP(edges, 1, np.pi, threshold=25, minLineLength=50, maxLineGap=10)
@@ -61,7 +66,7 @@ class FollowLine:
                 x1, y1, x2, y2 = line[0]
                 cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
-            cv2.imshow('final', img)
+            cv2.imshow('final', big)
             cv2.waitKey(5000)
         return lines
 
