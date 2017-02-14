fda1e996a4dabd1ac092d317fd5fede7e9518c21
apockill <apocthiel@gmail.com>
2017-02-13 20:50:47 -0700

    Transfering code to RasPi

--- Behaviors.py
+++ Behaviors.py
@@ -67,7 +67,7 @@ class FollowLine:
 
     def __combineLines(self, lines):
         """ Combines similar lines into one large 'average' line """
-        maxAngle = 25
+        maxAngle = 60
 
         def lineFits(checkLine, combo):
 
