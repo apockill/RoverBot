76015c37a4d8813f3252af051f6c74db1edf62ce
apockill <apocthiel@gmail.com>
2017-02-13 18:42:21 -0700

    Transfering code to RasPi

--- Behaviors.py
+++ Behaviors.py
@@ -32,8 +32,9 @@ class FollowLine:
 
 
         small = cv2.resize(rThresh,    (9, 12), interpolation=cv2.INTER_AREA)
-        big   = cv2.resize(  small, (640, 480), interpolation=cv2.INTER_AREA)  # TODO: Delete- for debug only
 
+        # Debug
+        big   = cv2.resize(  small, (640, 480), interpolation=cv2.INTER_AREA)  #Delete- for debug only
         cv2.imshow(   'frame', big)
         cv2.waitKey(5000)
 
@@ -41,8 +42,6 @@ class FollowLine:
 
 
     def __findLines(self):
-
-
         img   = self.rover.camera.read()
 
         rImg  = VisionUtils.isolateColor(img,   [150, 75, 75],  [30, 255, 255])
@@ -50,11 +49,17 @@ class FollowLine:
         ret, rThresh = cv2.threshold(rGray, 50, 255, cv2.THRESH_BINARY)
         edges = cv2.Canny(rThresh, 20, 40)
 
-        cv2.imshow('Thresh', rThresh)
+        # cv2.imshow('Thresh', rThresh)
 
 
         # lines = cv2.HoughLinesP(edges, 1, np.pi, threshold=25, minLineLength=50, maxLineGap=10)
         lines = cv2.HoughLinesP(edges, 1, np.pi/500, threshold=50, minLineLength=100, maxLineGap=100)
 
+        if lines is not None:
+            print("Length:", lines)
+            for line in lines[0]:
+                x1, y1, x2, y2 = line
+                cv2.line(rThresh, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.imshow(rThresh)
         return lines
 
