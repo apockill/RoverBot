6bd1743e642c85fc33da781fe8486fb4e65f9e9d
apockill <apocthiel@gmail.com>
2017-02-27 15:43:36 -0700

    Transfering code to RasPi

--- Behaviors.py
+++ Behaviors.py
@@ -93,7 +93,7 @@ class FollowLine:
                       for combo in lineCombos]
 
         # Limit each combo to just the minimum amount of samples required
-        lineCombo = [[combo[:minLinesForCombo]] for combo in lineCombos]
+        lineCombos = [combo[:minLinesForCombo] for combo in lineCombos]
 
 
 
