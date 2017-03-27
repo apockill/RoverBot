921b6bc4b73df28780ded22b4821dbb7aae92e31
apockill <apocthiel@gmail.com>
2017-03-27 16:45:22 -0700

    Sending code to RasPi

--- Behaviors/LineBehavior.py
+++ Behaviors/LineBehavior.py
@@ -64,7 +64,7 @@ class FollowLine:
     def __init__(self, parent):
         self.rover = parent
         self.map   = Mapper()
-        self.targetSpeed = 300
+        self.targetSpeed = 200
 
     def update(self):
         lowRed  = [150, 75, 75]
