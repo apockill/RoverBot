bc8de26096f53763606ec03fc793302d332de958
apockill <apocthiel@gmail.com>
2017-03-27 16:14:41 -0700

    Sending code to RasPi

--- HardwareLibs/Wheel.py
+++ HardwareLibs/Wheel.py
@@ -230,8 +230,8 @@ class Encoder:
         self.log.append(firstEntry)
 
         # Set up GPIO Events (after having gotten the values!) High bouncetime causes issues.
-        GPIO.add_event_detect(pinA, GPIO.BOTH, callback=self.pinChangeEvent, bouncetime=1)
-        GPIO.add_event_detect(pinB, GPIO.BOTH, callback=self.pinChangeEvent, bouncetime=1)
+        # GPIO.add_event_detect(pinA, GPIO.BOTH, callback=self.pinChangeEvent, bouncetime=1)
+        # GPIO.add_event_detect(pinB, GPIO.BOTH, callback=self.pinChangeEvent, bouncetime=1)
 
     def pinChangeEvent(self, pin):
         # Find the pin that has been flipped, then act accordingly
