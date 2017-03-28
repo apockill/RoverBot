2f68e1b3b4748e7d8e9922687b4b3592c606f21d
apockill <apocthiel@gmail.com>
2017-03-28 16:04:28 -0700

    Sending code to RasPi

--- Behaviors/LineBehavior.py
+++ Behaviors/LineBehavior.py
@@ -108,8 +108,8 @@ class FollowLine:
         # Speed: From targetSpeed*.5 to targetSpeed, where 0 y leads to .5 targetSpeed
         speed = (self.targetSpeed * .5) * yMag  # clamp(self.targetSpeed * yMag, self.targetSpeed*.5, self.targetSpeed)
 
-        left  = self.targetSpeed*.5 + speed * xMag  # Where -1 xmag will lower left turning speed
-        right = self.targetSpeed*.5 - speed * xMag  # Where -1 xmag will raise the right turning speed
+        left  = self.targetSpeed*.35 + speed * xMag  # Where -1 xmag will lower left turning speed
+        right = self.targetSpeed*.35 - speed * xMag  # Where -1 xmag will raise the right turning speed
         left  = int(left)
         right = int(right)
         lWheel.setSpeed(left)
