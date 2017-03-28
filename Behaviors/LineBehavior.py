89481b6d71f4a1b93bbfc0a22bfc36b17ba3be98
apockill <apocthiel@gmail.com>
2017-03-28 14:05:20 -0700

    Sending code to RasPi

--- Behaviors/LineBehavior.py
+++ Behaviors/LineBehavior.py
@@ -104,9 +104,15 @@ class FollowLine:
 
         if highestPoint[0] < horzMiddle:
             print("Left")
+            lWheel.setSpeed(self.targetSpeed * .5)
+            rWheel.setSpeed(self.targetSpeed)
+            return
 
         if highestPoint[0] > horzMiddle:
             print("Right")
+            lWheel.setSpeed(self.targetSpeed)
+            rWheel.setSpeed(self.targetSpeed * .5)
+            return
 
         """
         lowerThresh = 89
@@ -227,29 +233,28 @@ class FollowLine:
             averagedCombos.append(Line(avgLine[:2], avgLine[2:]))
 
 
-
-        # Draw Line Combos and Final Lines
-        img = self.rover.camera.read()
-        for i, combo in enumerate(lineCombos):
-            for x1, y1, x2, y2 in combo:
-                x1 *= 10
-                y1 *= 10
-                x2 *= 10
-                y2 *= 10
-
-                cv2.line(img, (x1, y1), (x2, y2), (80*i, 80*i, 80*i), 2)
-
-        if len(averagedCombos):
-            for p1, p2 in averagedCombos:
-                x1 = p1[0]
-                y1 = p1[1]
-                x2 = p2[0]
-                y2 = p2[1]
-
-                cv2.line(img, (x1, y1), (x2, y2), (80, 80, 80), 8)
-
-        cv2.imshow('final', img)
-        cv2.waitKey(2500)
+        # # Draw Line Combos and Final Lines
+        # img = self.rover.camera.read()
+        # for i, combo in enumerate(lineCombos):
+        #     for x1, y1, x2, y2 in combo:
+        #         x1 *= 10
+        #         y1 *= 10
+        #         x2 *= 10
+        #         y2 *= 10
+        #
+        #         cv2.line(img, (x1, y1), (x2, y2), (80*i, 80*i, 80*i), 2)
+        #
+        # if len(averagedCombos):
+        #     for p1, p2 in averagedCombos:
+        #         x1 = p1[0]
+        #         y1 = p1[1]
+        #         x2 = p2[0]
+        #         y2 = p2[1]
+        #
+        #         cv2.line(img, (x1, y1), (x2, y2), (80, 80, 80), 8)
+        #
+        # cv2.imshow('final', img)
+        # cv2.waitKey(2500)
 
         return averagedCombos
 
