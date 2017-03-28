8c9987485b12ec0d9e1f9c0dd7b858c39407f18c
apockill <apocthiel@gmail.com>
2017-03-28 14:14:38 -0700

    Sending code to RasPi

--- Main.py
+++ Main.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     print("\n\nStarting!\n")
     robot  = RoverHandler()
     robot.mainThread()
-
+    robot.LWheel.setSpeed(250)
     # print("Final L: ", robot.LWheel.encoder.getVelocity(sampleSize=50))
     # print("Final R: ", robot.RWheel.encoder.getVelocity(sampleSize=50))
 
